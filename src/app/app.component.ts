554083ed87579fec915d79481c305a859951ae7e
JosielJrr <josieljrr.dev@gmail.com>
2024-11-26 12:22:34 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from "./pages/home/home.component";
+import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+	selector: 'app-root',
+	imports: [RouterOutlet, HomeComponent, MenuBarComponent],
+	templateUrl: './app.component.html',
+	styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'playstation-store-angular';
+	title = 'playstation-store-angular';
 }
